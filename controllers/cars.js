0cf887c416d5c92537d385eab71efe51cd028de8
adicerbo <dicerboantonio32@gmail.com>
2022-09-12 12:23:01 -0400

    make basic boilerplate for each views file

--- controllers/cars.js
+++ controllers/cars.js
@@ -3,5 +3,21 @@ const express = require("express");
 const carRouter = express.Router();
 const Car = require("../models/cars.js")
 
+// index
+
+// new
+
+// delete
+
+// update
+
+// create
+
+// edit
+
+// show
+
+
+
 // exports
 module.exports = carRouter;
