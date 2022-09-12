914850bb8173f3ac6e22894b61a458e2cb8227c4
adicerbo <dicerboantonio32@gmail.com>
2022-09-12 11:57:36 -0400

    create mongoose schema

--- models/cars.js
+++ models/cars.js
@@ -1,1 +1,18 @@
 // dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// car schema
+const carSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    make: {type: String, required: true},
+    year: {type: Number, required: true},
+    description: {type: String, required: false},
+    price: {type: Number, required: false},
+    image: {type: String, required: false},
+    link: {type: String, required: false}
+})
+
+const Car = mongoose.model("Car", carSchema);
+
+module.exports = Car;
