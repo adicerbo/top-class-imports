2e1ec78e34c7f1b5963184aed75c373828320c1b
adicerbo <dicerboantonio32@gmail.com>
2022-09-15 16:45:44 -0400

    update code for heroku

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 // dependencies
 const express = require("express");
-const app = express();
-const mongoose = require('mongoose');
 const methodOverride = require("method-override")
+const mongoose = require('mongoose');
+const app = express();
+const db = mongoose.connection;
 const carController = require("./controllers/cars.js")
 
+const PORT = process.env.PORT || 3000;
+
+// connect to database via heroku/locally
+const MONGODB_URI = process.env.MONGODB_URI;
+
 // database configuration
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(MONGODB_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
-});
+}
+);
 
 // database connection error / success
-const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
@@ -29,5 +35,4 @@ app.use("/cars", carController);
 const Car = require("./models/cars.js");
 
 // listener
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`server is listening on port ${PORT}.`))
+app.listen(PORT, () => console.log(`server is listening on port`, PORT))
